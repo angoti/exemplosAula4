0a9f915012c2571516444de8c93cbe33b60a5946
Edson Angoti Júnior <angoti@iftm.edu.br>
2017-09-27 18:31:45 -0300

    Exemplos da aula 4

---
+++ src/ocp/segundo/Frete.java
@@ -0,0 +1,10 @@
+package ocp.segundo;
+
+public class Frete implements ServicoDeEntrega {
+
+	public double para(String cidade) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ src/ocp/segundo/Teste.java
@@ -0,0 +1,5 @@
+package ocp.segundo;
+
+public class Teste {
+
+}
